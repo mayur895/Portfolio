75bd983b8955c69cb653e49fdc8dfc8f147ffd53
Mayur Singh <officialmayur51@gmail.com>
2026-03-28 18:40:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //event listener  logic is running
+    //event listener  lo
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
