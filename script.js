c737892f1404e8ef62831f3e5df6593093524027
Mayur Singh <officialmayur51@gmail.com>
2026-04-18 10:18:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
     document.querySelectorAll('.hidden-element').forEach((el) => observer.observe(el));
   
-    // Smooth scrolling for navigation yes it is
+    // Smooth scrolling for navigation yes it is iuvgm
     function scrollToSection(id) {
       const element = document.getElementById(id);
       if (element) {
