2f97c80549cad2ec835c28b0fb6d53202d5680bd
Mayur Singh <officialmayur51@gmail.com>
2026-04-23 19:45:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //event listener  
+    //event listener   jlnjjj
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
